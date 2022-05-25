713e2bc4b6a1ca5d3553ad13115a6f707ee0d844
alsenaniDev <alsenaniMohammed1@gmail.com>
2022-05-25 10:24:01 +0300

    Recovering for add-users page, admin-page, alert, delete-model and navbar

--- src/app/components/admin-page/admin-page.component.ts
+++ src/app/components/admin-page/admin-page.component.ts
@@ -9,16 +9,16 @@ export class AdminPageComponent implements OnInit {
   users = JSON.parse(localStorage.getItem("UsersDB") || "[]")
   userProfile = JSON.parse(localStorage.getItem("userInfo") || "null")
   usersInfo = JSON.parse(localStorage.getItem("usersInfoDB") || "[]")
-  userFound = this.usersInfo.find((user : any) => user.userId == this.userProfile.userId)
- 
+  userFound = this.usersInfo.find((user: any) => user.userId == this.userProfile.userId)
+
 
   constructor() { }
 
   ngOnInit(): void {
 
   }
+
   checkRole(name: string) {
     return this.userProfile.role == name;
   }
-
 }
--- src/app/components/users/add-users/add-users.component.ts
+++ src/app/components/users/add-users/add-users.component.ts
@@ -1,7 +1,8 @@
-import { Component, Injectable, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { Component, Injectable } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Guid } from "guid-typescript"
+
 declare var $: any;
 @Component({
   selector: 'app-add-users',
@@ -10,18 +11,17 @@ declare var $: any;
 })
 
 @Injectable({ providedIn: "root" })
+
 export class AddUsersComponent {
   SignUpForm: FormGroup;
+  
   groups = JSON.parse(localStorage.getItem("GroupsDB") || "[]")
   roles = JSON.parse(localStorage.getItem("RolesDB") || '[]')
   Users = JSON.parse(localStorage.getItem("UsersDB") || "[]")
   userProfile = JSON.parse(localStorage.getItem("userInfo") || "null")
   usersInfo = JSON.parse(localStorage.getItem("usersInfoDB" || "[]"))
+
   usersEmail = this.Users.map((user: any) => user.email)
-  groupsList: any = [...this.groups]
-  rolesList: any = [...this.roles]
-  usersList: any = [...this.Users]
-  usersInfoList: any = [...this.usersInfo]
 
   controls = [
     {
@@ -63,25 +63,26 @@ export class AddUsersComponent {
       role: ['', Validators.required],
     });
   }
+
   ngOnInit(): void {
   }
-
-
 
   onSubmit() {
     if (this.SignUpForm.invalid) {
       this.SignUpForm.markAllAsTouched()
-
-    } else if (this.usersEmail.includes(this.SignUpForm.value.email)) {
+    }
+    else if (this.usersEmail.includes(this.SignUpForm.value.email)) {
       this.SignUpForm.invalid
-    } else {
+    }
+    else {
       let guid = Guid.create().toJSON();
       let userGuid = guid.value
       let dateObj = new Date();
       let month = dateObj.getMonth() + 1;
       let day = dateObj.getDate();
       let year = dateObj.getFullYear();
-      let newdate = year + "-" + month + "-" + day;
+      let newDate = year + "-" + month + "-" + day;
+
       let user = {
         userId: userGuid,
         firstName: this.SignUpForm.value.fname,
@@ -90,20 +91,25 @@ export class AddUsersComponent {
         password: this.SignUpForm.value.password,
         phoneNumber: this.SignUpForm.value.phoneNumber,
         CreatedBy: this.userProfile.userId,
-        CreatedAt: newdate
+        CreatedAt: newDate
       }
-      this.usersList.push(user)
-      localStorage.setItem("UsersDB", JSON.stringify(this.usersList))
+
+      this.Users.push(user)
+
+      localStorage.setItem("UsersDB", JSON.stringify(this.Users))
+
       const userInfo = {
         userId: userGuid,
         role: this.SignUpForm.value.role,
         groups: this.SignUpForm.value.groups
       }
-      this.usersInfoList.push(userInfo)
-      localStorage.setItem("usersInfoDB", JSON.stringify(this.usersInfoList))
-      this.usersInfoList = this.usersInfo
+
+      this.usersInfo.push(userInfo)
+      localStorage.setItem("usersInfoDB", JSON.stringify(this.usersInfo))
+      this.usersInfo = this.usersInfo
+
       this.SignUpForm.reset()
-      this.router.navigateByUrl("/dashboard/admin")
+      this.router.navigateByUrl("/dashboard/show")
     }
   }
 }
