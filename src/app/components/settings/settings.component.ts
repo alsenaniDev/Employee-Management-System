ac6cac26bd8234166666d38f33e185f9affc3469
Mohammed-Dukhaykh <Mohammed.Obaidallah.abt.js@tuwaiq.edu.sa>
2022-05-26 00:35:58 +0300

    Refactoring Setting Componet and Create Show Modal Componet

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ShowModalComponent } from '../common/show-modal/show-modal.component';
 
 @Component({
   selector: 'app-settings',
@@ -6,14 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-  data: any = [];
-  isEdit: boolean = false;
-  elementId: string = "";
+  Groups = JSON.parse(localStorage.getItem("GroupsDB") || "[]")
+  Roles = JSON.parse(localStorage.getItem("RolesDB") || "[]")
   dataName: string = "";
+  showModal : any
+
   constructor() { }
+  @ViewChild(ShowModalComponent) modalShow: ShowModalComponent
 
   ngOnInit(): void {
-    this.getCounts();
+
+    }
+  ngAfterViewInit(): void {
+    this.showModal 
+    
   }
 
   showModel(name: string) {
@@ -22,139 +29,9 @@ export class SettingsComponent implements OnInit {
     document.getElementById("label-name")!.innerHTML = "Name of " + name
     document.getElementById("label-table")!.innerHTML = name + " Table"
     this.dataName = name;
-    this.getData(name)
+    this.modalShow.getData(name)
+    
+    
   }
 
-  AddData() {
-    var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-
-    if ((<HTMLInputElement>document.getElementById("inputValue")).value != "") {
-      if (this.isEdit === false) {
-        let name = document.getElementById("model-title")!.innerHTML;
-        let dataName = name.split(" ");
-
-        if (dataName[0] == "Groups") {
-          let groups = JSON.parse(localStorage.getItem("GroupsDB") || 'null');
-          console.log(groups);
-          if (groups == null || groups.length <= 0) {
-            let newGroups = [{
-              id: 1,
-              name: (<HTMLInputElement>document.getElementById("inputValue")).value,
-              createBy: localStorage.getItem("token"),
-              createAt: date
-            }];
-            localStorage.setItem("GroupsDB", JSON.stringify(newGroups));
-          } else {
-            let groups = JSON.parse(localStorage.getItem("GroupsDB") || '');
-            let lastItem = groups[groups.length - 1];
-            let newItem = {
-              id: lastItem.id + 1,
-              name: (<HTMLInputElement>document.getElementById("inputValue")).value,
-              createBy: localStorage.getItem("token"),
-              createAt: date
-            };
-            groups.push(newItem);
-            localStorage.setItem("GroupsDB", JSON.stringify(groups));
-          }
-        } else if (dataName[0] == "Roles") {
-          let roles = JSON.parse(localStorage.getItem("RolesDB") || '');
-          if (roles == null || roles.length <= 0) {
-            let newRoles = [{
-              id: 1,
-              name: (<HTMLInputElement>document.getElementById("inputValue")).value,
-              createBy: localStorage.getItem("token"),
-              createAt: date
-            }];
-            localStorage.setItem("RolesDB", JSON.stringify(newRoles));
-          } else {
-            let roles = JSON.parse(localStorage.getItem("RolesDB") || '');
-            let lastItem = roles[roles.length - 1];
-            let newItem = {
-              id: lastItem.id + 1,
-              name: (<HTMLInputElement>document.getElementById("inputValue")).value,
-              createBy: localStorage.getItem("token"),
-              createAt: date
-            };
-            roles.push(newItem);
-            localStorage.setItem("RolesDB", JSON.stringify(roles));
-          }
-        }
-
-        (<HTMLInputElement>document.getElementById("inputValue")).value = "";
-        this.getCounts();
-        this.getData(dataName[0]);
-        document.getElementById("alert")!.style.display = "none";
-      } else {
-        let name = document.getElementById("model-title")!.innerHTML;
-        let dataName = name.split(" ");
-
-        if (dataName[0] == "Groups") {
-          let groups = JSON.parse(localStorage.getItem("GroupsDB") || '');
-          groups.find((x: any) => x.id == this.elementId).name = (<HTMLInputElement>document.getElementById("inputValue")).value;
-          localStorage.setItem("GroupsDB", JSON.stringify(groups));
-        } else if (dataName[0] == "Roles") {
-          let roles = JSON.parse(localStorage.getItem("RolesDB") || '');
-          roles.find((x: any) => x.id == this.elementId).name = (<HTMLInputElement>document.getElementById("inputValue")).value;
-          localStorage.setItem("RolesDB", JSON.stringify(roles));
-        }
-
-        (<HTMLInputElement>document.getElementById("inputValue")).value = "";
-        this.getCounts();
-        this.getData(dataName[0]);
-        this.isEdit = false;
-        document.getElementById("alert")!.style.display = "none";
-      }
-    } else {
-      document.getElementById("alert")!.style.display = "";
-    }
-  }
-
-  getCounts() {
-    let countGroups = (JSON.parse(localStorage.getItem("GroupsDB") || 'null'));
-    if (countGroups != null) {
-      document.getElementById("groups-count")!.innerHTML = countGroups.length;
-    } else {
-      document.getElementById("groups-count")!.innerHTML = "0";
-    }
-
-    let countRoles = (JSON.parse(localStorage.getItem("RolesDB") || 'null'));
-    if (countRoles != null) {
-      document.getElementById("roles-count")!.innerHTML = countRoles.length;
-    } else {
-      document.getElementById("roles-count")!.innerHTML = "0";
-    }
-  }
-
-  getData(name: string) {
-    this.data = [];
-    this.data = JSON.parse(localStorage.getItem(name + "DB") || 'null')
-    if (this.data != null) {
-      this.data = JSON.parse(localStorage.getItem(name + "DB") || 'null')
-    } else {
-      this.data = [];
-    }
-  }
-
-  DeleteElement(id: string) {
-    let name = document.getElementById("model-title")!.innerHTML;
-    let dataName = name.split(" ");
-    let data = (JSON.parse(localStorage.getItem(dataName[0] + "DB") || ''));
-
-    this.data = data.filter((x: any) => x.id != id)
-    localStorage.setItem(dataName[0] + "DB", JSON.stringify(this.data));
-    this.getCounts();
-    this.isEdit = false
-  }
-
-  EditElement(id: string) {
-    let name = document.getElementById("model-title")!.innerHTML;
-    let dataName = name.split(" ");
-    let data = (JSON.parse(localStorage.getItem(dataName[0] + "DB") || ''));
-
-    let element = data.find((x: any) => x.id == id);
-    (<HTMLInputElement>document.getElementById("inputValue")).value = element.name;
-    this.elementId = element.id;
-    this.isEdit = true;
-  }
 }
